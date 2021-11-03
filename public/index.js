59d3236b1d6c3e854d9f0698463adbe112ebbd5e
unknown <sjxliu8@gmail.com>
2021-11-03 17:57:13 -0500

    tears

--- public/index.js
+++ public/index.js
@@ -1,9 +1,26 @@
+const { GME, MSFT, DIS, BNTX } = mockData;
+
+const stocks = [GME, MSFT, DIS, BNTX];
+
+console.log(stocks)
+
+
+
+
 async function main() {
 
+    const response = await fetch ("https://api.twelvedata.com/api_usage?apikey=5aa70df3474f411cbe998e4e5a92af11");
+        const charts = await response.json();
+       return charts;
+ 
+}
+
+async function renderCharts(chart) {
     const timeChartCanvas = document.querySelector('#time-chart');
     const highestPriceChartCanvas = document.querySelector('#highest-price-chart');
     const averagePriceChartCanvas = document.querySelector('#average-price-chart');
-
 }
+    
+
 
 main()
